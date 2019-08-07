e227329ffb856a81fdaca02c43250984648607f0
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 04:01:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
 retrieve_element_from_index(array, index_number) =  ["Sam", "Anthony", "Lance", "Matt"]
-retrieve_element_from_index(array, index_number)
+retrieve_element_from_index(array, index_number)[0]
 end
 
 def retrieve_first_element_from_array(array)

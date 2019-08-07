d41fcfd71252fab0888d8d8980ed99db1c44a048
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 03:43:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ create_an_array = ["Sam", "Anthony", "Lance", "Matt"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+create_an_array << ["Stocker"]
 end
 
 def add_element_to_start_of_array(array, element)

8a0f27fc327db01f27a47d3f341df63665451a23
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 03:54:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ add_an_element_to_end_of_array.push( "Stocker" )
 end
 
 def add_element_to_start_of_array(array, element)
-
+add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
